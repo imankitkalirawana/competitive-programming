e47de68941b7c3626b8489d314e8672957faa52b
Bhuneshvar <imankitkalirawana@gmail.com>
2025-02-02 20:59:58 +0530

    Add functions for binary addition, climbing stairs, and calculating square root

---
+++ climb-stairs.js
@@ -0,0 +1,13 @@
+var climbStairs = function (n) {
+    if (n <= 2) return n;
+    let first = 1;
+    let second = 2;
+    for (let i = 3; i <= n; i++) {
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    return second;
+};
+
+console.log(climbStairs(2)) // 2
